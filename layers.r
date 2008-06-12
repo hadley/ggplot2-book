55eec91cb797e7eaeb15d31f6303c1d346ec544f
hadley <h.wickham@gmail.com>
2008-06-11 21:51:08 -0500

    Working away

---
+++ layers.r
@@ -0,0 +1,14 @@
+source("latex.r")
+source("~/Documents/eval.with.details/eval.with.details/R/weave-tex.r")
+
+aesthetics <- function(o) sort(c(names(o$default_aes()), o$required_aes))
+params <- function(x) sort(setdiff(names(x$parameters()), c(aesthetics(x), "...")))
+output <- function(o) sort(names(o$desc_outputs))
+comma <- function(x) paste(escape_tex(x), collapse= ", ")
+
+ap <- function(x) paste(comma(aesthetics(x)), comma(params(x)), sep = " + ")
+
+cat(tabulate(ldply(Geom$find_all(), function(c) c(escape_tex(c$objname), escape_tex(c$desc), ap(c))))  )
+
+cat(tabulate(ldply(Stat$find_all(), function(c) c(escape_tex(c$objname), escape_tex(c$desc), comma(params(c)), comma(aesthetics(c)), comma(output(c))))))
+
