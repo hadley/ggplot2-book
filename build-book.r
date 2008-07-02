a8c25ed62bcab4074bd78caf88107e4a9e834770
hadley <h.wickham@gmail.com>
2008-07-01 20:09:31 -0500

    Lots of work on book

--- build-book.r
+++ build-book.r
@@ -1,3 +1,10 @@
+l(plyr)
+l(decumar)
+tex <- dir("\\.tex$")
+
+l_ply(tex, overwrite_file)
+
+
 /Applications/TextMate.app/Contents/SharedSupport/Bundles/Latex.tmbundle/Support/bin/latexmk.pl -pdf -f [^_]*.tex 
 
 cp *.pdf public
