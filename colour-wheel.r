b7bee9ec0c93f6554917ca96618b8fca53759758
hadley <h.wickham@gmail.com>
2008-08-11 11:00:03 -0500

    Colour wheel for mastery chapter

--- colour-wheel.r
+++ colour-wheel.r
@@ -1,21 +1,21 @@
-hcl <- expand.grid(h = seq(0, 360, by = 2), l = 50, c = seq(0, 100, by = 2))
-hcl <- transform(hcl, 
-  angle = h * pi / 180,
-  radius = c / diff(range(c))
-)
-hcl <- transform(hcl,
-  x = radius * sin(angle),
-  y = radius * cos(angle)
-)
+hcl <- expand.grid(x = seq(-1, 1, length = 100), y = seq(-1, 1, length=100))
+hcl <- subset(hcl, x^2 + y^2 < 1)
+
+hcl <- within(hcl, {
+  r <- sqrt(x^2 + y^2)
+  
+  c <- 100 * r
+  h <- 180 / pi * atan2(x, y)
+  l <- 65
+})
+
 hcl$colour <- hcl(hcl$h, hcl$c, hcl$l)
 
-
+angles <- seq(0, 2*pi, length = 6)[-6]
+selected <- data.frame(x = sin(angles), y = cos(angles))
 
 # with(hcl, plot(x, y, col=colour, pch=20, cex=3))
-# print(qplot(x, y, data=hcl, colour=colour) + scale_colour_identity())
+# with(selected, points(x, y, cex = 3))
 
-
-rgb_vals <- expand.grid(r = seq(0, 1, length = 10), g = seq(0, 1, length=10), b = 0.5)
-rgb_vals$colour <- do.call(rgb, rgb_vals)
-
-with(rgb_vals, plot(r, g, col=colour, pch=20, cex=5))
+qplot(x, y, data=hcl, colour=colour) + scale_colour_identity() + coord_equal() + scale_x_continuous("", breaks=NA) + scale_y_continuous("", breaks=NA) + geom_point(aes(colour = NULL), selected, size = 5, shape = 1)
+ggsave(file = "colour-wheel.pdf", width = 6, height = 6)
