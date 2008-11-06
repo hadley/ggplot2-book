fb57054693a9916756b510f2bd95d06e0546a6e5
hadley <h.wickham@gmail.com>
2008-11-06 08:37:57 -0600

    More work, particularly to toolbox and writing your own

--- build-book.r
+++ build-book.r
@@ -4,7 +4,7 @@ l(ggplot)
 
 # Need to clean out include directory
 
-chapters <- c("introduction", "layers", "mastery", "position", "polishing", "qplot", "scales", "specifications", "toolbox", "book-mine")
+chapters <- c("introduction", "layers", "mastery", "position", "polishing", "qplot", "scales", "specifications", "toolbox", "book-mine", "writing-your-own")
 
 tex <- paste(chapters, ".tex", sep="")
 r <-   file.path("public", paste(chapters, ".r", sep=""))
