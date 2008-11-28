4d89e502fc69da21096e1d5cd2fba0059add8922
hadley <h.wickham@gmail.com>
2008-11-28 12:57:17 -0600

    Integrate changes from paper copy

--- layers.r
+++ layers.r
@@ -1,5 +1,5 @@
 source("latex.r")
-source("~/Documents/eval.with.details/eval.with.details/R/weave-tex.r")
+l(decumar)
 
 aesthetics <- function(o) sort(c(names(o$default_aes()), o$required_aes))
 params <- function(x) sort(setdiff(names(x$parameters()), c(aesthetics(x), "...")))
@@ -8,7 +8,19 @@ comma <- function(x) paste(escape_tex(x), collapse= ", ")
 
 ap <- function(x) paste(comma(aesthetics(x)), comma(params(x)), sep = " + ")
 
-cat(tabulate(ldply(Geom$find_all(), function(c) c(escape_tex(c$objname), escape_tex(c$desc))))  )
+g <- GeomPoint
+aesthetics2 <- function(g) {
+  a <- aesthetics(g)
+  req <- a %in% g$required_aes
+  
+  a[req] <- paste("\\textbf{", a[req], "}", sep = "")
+  paste(a, collapse = ", ")
+}
+
+cat(tabulate(ldply(Geom$find_all(), function(c) c(escape_tex(c$objname), escape_tex(c$desc)))))
+
+cat(tabulate(ldply(Geom$find_all(), function(c) c(escape_tex(c$objname),escape_tex(c$default_stat()$objname), aesthetics2(c))))  )
+
 
 cat(tabulate(ldply(Stat$find_all(), function(c) c(escape_tex(c$objname), escape_tex(c$desc)))))
 
