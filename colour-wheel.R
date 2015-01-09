7d12bb5d0546baa2fbcc6a9f937aeae088ad73d6
hadley <h.wickham@gmail.com>
2015-01-09 13:57:48 -0600

    Fix up colour wheel

--- colour-wheel.R
+++ colour-wheel.R
@@ -1,17 +1,34 @@
-hcl <- expand.grid(x = seq(-1, 1, length = 100), y = seq(-1, 1, length=100))
-hcl <- subset(hcl, x^2 + y^2 < 1)
-hcl <- within(hcl, {
-  r <- sqrt(x^2 + y^2)
-  c <- 100 * r
-  h <- 180 / pi * atan2(x, y)
-  l <- 65
-})
-hcl$colour <- hcl(hcl$h, hcl$c, hcl$l)
-rgb_vals <- expand.grid(r = seq(0, 1, length = 10), g = seq(0, 1, length=10), b = 0.5)
-rgb_vals$colour <- do.call(rgb, rgb_vals)
+library(scales)
+library(dplyr)
+library(colorspace)
 
-# how to get the 'selected' data for this plot?
-# qplot(x, y, data=hcl, colour=colour) + scale_colour_identity() + coord_equal() + scale_x_continuous("", breaks=NA) + scale_y_continuous("", breaks=NA) + geom_point(aes(colour = NULL), selected, size = 5, shape = 1)
+hcl <- expand.grid(x = seq(-1, 1, length = 100), y = seq(-1, 1, length=100)) %>% 
+  tbl_df() %>% 
+  filter(x^2 + y^2 < 1) %>%
+  mutate(
+    r = sqrt(x^2 + y^2),
+    c = 100 * r,
+    h = 180 / pi * atan2(y, x),
+    l = 65,
+    colour = hcl(h, c, l)
+  )
 
-# doesn't quite give me what I want...
-# selected <- subset(hcl, colour %in% hue_pal()(5))
+# sin(h) = y / (c / 100)
+# y = sin(h) * c / 100
+
+selected <- RGB(t(col2rgb(hue_pal()(5))) / 255) %>%
+  as("polarLUV") %>%
+  coords() %>%
+  as.data.frame() %>%
+  mutate(
+    x = cos(H / 180 * pi) * C / 100,
+    y = sin(H / 180 * pi) * C / 100
+  )
+
+ggplot(hcl, aes(x, y)) + 
+  geom_raster(aes(fill = colour)) + 
+  scale_fill_identity() + 
+  coord_equal() + 
+  scale_x_continuous("", breaks = NULL) + 
+  scale_y_continuous("", breaks = NULL) + 
+  geom_point(data = selected, size = 5, shape = 1)
