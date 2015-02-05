a1e2eeb8d6397c5efba7be3eb5f364330b312c29
Anonymous@example.com <cpsievert1@gmail.com>
2015-02-05 15:18:21 -0600

    Test out captioner on qplot

--- render-tex.R
+++ render-tex.R
@@ -1,6 +1,7 @@
 library("bookdown")
 library("rmarkdown")
 library("xtable")
+library("captioner")
 library("methods")
 
 # Set global options for tables and precompile time-intensive tables -----------
@@ -37,6 +38,37 @@ render_chapter <- function(src) {
                  file = "book/tex/render-log.txt")
 }
 
+# list the chapters in order they should appear
+# this is necessary to handle figure/table bumping properly
+chap_order <- c("introduction", "qplot", "mastery", "layers", "toolbox",
+                "scales", "position", "polishing", "data", "duplication",
+                "translating", "specifications", "grid")
+chap_ord <- paste0(chap_order, ".rmd")
+if (!setequal(chap_ord, dir(pattern = ".rmd"))) 
+  warning("Detected a difference in actual and expected chapters.")
+
+# command line arguments should be the chapter names
 args <- commandArgs(trailingOnly = TRUE)
-chaps <- dir(pattern = ".rmd")
-lapply(args[args %in% chaps], render_chapter)
+# Chapter "id numbers"  (used to 'bump' figure/table numbering)
+chap_id <- sort(match(args, chap_ord))
+# The number of times to 'bump' before rendering each chapter
+bump_n <- c(chap_id[1], diff(chap_id))
+
+# Initiate captioner functions
+figs <- captioner(levels = 2, type = c("n", "n"), infix = ".")
+tbls <- captioner(prefix = "Table", 
+                  levels = 2, type = c("n", "n"), infix = ".")
+# Bump figure/table numbering a specified number of times *before*
+# rendering a chapter
+bumper <- function(times, chap, ...) {
+  if (times == 0) return(invisible())
+  for (i in seq_len(times)) {
+    bump(figs, level = 1)
+    bump(tbls, level = 1)
+  }
+  render_chapter(chap)
+}
+
+mapply(bumper, bump_n, chap_ord[chap_id], SIMPLIFY = FALSE)
+
+
