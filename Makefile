b8c10fd8603d921a8620582742e1e6540494c727
hadley <h.wickham@gmail.com>
2015-08-06 18:15:06 -0500

    Simplification and clean up of makefile

--- Makefile
+++ Makefile
@@ -1,30 +1,22 @@
 TEXDIR := book/tex
-RMD_CHAPTERS := $(shell find . -name '*.rmd')
+RMD_CHAPTERS := $(shell find . -name '*.rmd' -depth 1)
 TEX_CHAPTERS := $(patsubst ./%.rmd, $(TEXDIR)/%.tex, $(RMD_CHAPTERS))
 
 all: book/ggplot2-book.pdf
 
 book/ggplot2-book.pdf: $(TEXDIR)/ggplot2-book.pdf
 	cp $(TEXDIR)/ggplot2-book.pdf book/ggplot2-book.pdf
-	Rscript -e 'embedFonts("book/ggplot2-book.pdf")'
-	
+
 # compile tex to pdf
 $(TEXDIR)/ggplot2-book.pdf: $(TEXDIR)/ggplot2-book.tex $(TEXDIR)/krantz.cls $(TEX_CHAPTERS)
 	mkdir -p $(TEXDIR)/figures && cp -r figures/* $(TEXDIR)/figures
-	mkdir -p $(TEXDIR)/tbls && cp -r tbls/* $(TEXDIR)/tbls
 	mkdir -p $(TEXDIR)/diagrams && cp -r diagrams/* $(TEXDIR)/diagrams
-	# http://stackoverflow.com/questions/3148492/makefile-silent-remove
-	cd $(TEXDIR) && @rm ggplot2-book.ind 2> /dev/null || true
-	cd $(TEXDIR) && @rm ggplot2-book.out 2> /dev/null || true
-	cd $(TEXDIR) && xelatex ggplot2-book.tex
-	cd $(TEXDIR) && makeindex ggplot2-book.idx
-	cd $(TEXDIR) && xelatex ggplot2-book.tex 
-	cd $(TEXDIR) && xelatex ggplot2-book.tex
+	cd $(TEXDIR) && latexmk -xelatex -use-make -interaction=batchmode ggplot2-book.tex
 
 # copy over LaTeX templates and style files
-$(TEXDIR)/krantz.cls: book/krantz.cls $(TEXDIR)
+$(TEXDIR)/krantz.cls:
 	cp book/krantz.cls $(TEXDIR)/krantz.cls
-$(TEXDIR)/ggplot2-book.tex: book/ggplot2-book.tex $(TEXDIR)
+$(TEXDIR)/ggplot2-book.tex: book/ggplot2-book.tex
 	cp book/ggplot2-book.tex $(TEXDIR)/ggplot2-book.tex
 
 # rmd -> tex
@@ -36,5 +28,4 @@ $(TEXDIR):
 
 clean:
 	rm -r $(TEXDIR)
-	rm -r tbls
 	rm -r figures
--- render-tex.R
+++ render-tex.R
@@ -1,59 +1,21 @@
 library("bookdown")
 library("rmarkdown")
-library("xtable")
-library("captioner")
-library("methods")
+library("methods") # avoids weird broom error
 
-# Set global options for tables and precompile time-intensive tables -----------
-options(xtable.include.rownames = FALSE)
-options(xtable.comment = FALSE)
-
-# 'global' convenience functions -----------------------------------------------
-tex_code <- function(x) paste0("\\texttt{", x, "}")
-
-# generate tables, if necessary  -----------------------------------------------
-if (!file_test("-d", "tbls")) dir.create("tbls")
-# Is render-tbls.R newer than it's target? (essentially what make does)
-nt <- file_test("-nt", "render-tbls.R", list.files("tbls"))
-if (length(nt) == 0 || any(nt)) source("render-tbls.R")
-
-# Render chapters into tex  ----------------------------------------------------
-render_chapter <- function(src) {
-  dest <- file.path("book/tex/", gsub("\\.rmd", "\\.tex", src))
-  base <- bookdown::tex_chapter()
-  chap <- sub("\\.rmd", "", src)
-  # set some global knitr chunk options...
-  base$knitr$opts_chunk <- 
-    plyr::defaults(list(message = FALSE,
-                        warning = FALSE,
-                        fig.show = 'hold',
-                        fig.align = 'center',
-                        fig.height = 4,
-                        fig.width = 4,
-                        out.width = "0.49\\linewidth",
-                        fig.path = paste0("figures/", chap),
-                        cache.path = paste0("_cache/", chap)),
-                   base$knitr$opts_chunk)
-  capture.output(render(src, base, output_dir = "book/tex", envir = globalenv()),
-                 file = "book/tex/render-log.txt")
-}
-
-args <- commandArgs(trailingOnly = TRUE)
+path <- commandArgs(trailingOnly = TRUE)
 # command line args should contain just one chapter name
-stopifnot(length(args) == 1)
-chap <- sub("\\.[R-r]md", "", args)
-# this csv records the order in which the chapters should appear
-chaps <- read.csv("chapter_order.csv")
-idx <- chaps$chapter %in% chap
-if (!any(idx)) warning("Chapter ", chap, " not found in chapter_order.csv!")
-n <- chaps[idx, "num"]
+if (length(path) == 0) {
+  message("No input supplied")
+} else {
+  base <- bookdown::tex_chapter()
+  base$knitr$opts_chunk$fig.show <- "hold"
+  base$knitr$opts_chunk$fig.align <- "center"
+  base$knitr$opts_chunk$out.width <- "0.49\\linewidth"
 
-# Initiate captioner functions
-figs <- captioner(levels = 2, type = c("n", "n"), infix = ".")
-tbls <- captioner(prefix = "Table", levels = 2, type = c("n", "n"), infix = ".")
-# Bump the chapter number for figure/table referencing
-for (i in seq_len(n)) {
-  bump(figs, level = 1)
-  bump(tbls, level = 1)
+  chap <- sub("\\.rmd", "", path)
+  base$knitr$opts_chunk$fig.path <- paste0("figures/", chap, "/")
+  base$knitr$opts_chunk$cache.path <- paste0("_cache/", chap, "/")
+  base$knitr$opts_chunk$cache <- TRUE
+
+  render(path, base, output_dir = "book/tex", envir = globalenv(), quiet = TRUE)
 }
-render_chapter(args)
