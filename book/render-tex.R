276dc1bc57dcac0c228fc03347c23e02914cd22d
Danielle Navarro <d.navarro@unsw.edu.au>
2019-06-14 21:21:19 +1000

    tidy whitespace

--- book/render-tex.R
+++ book/render-tex.R
@@ -6,8 +6,14 @@ tex_chapter <- function (chapter = NULL, latex_engine = c("xelatex", "pdflatex",
   options(digits = 3)
   set.seed(1014)
   latex_engine <- match.arg(latex_engine)
-  rmarkdown::output_format(rmarkdown::knitr_options("html", chapter), rmarkdown::pandoc_options(to = "latex",
-                                                                                                from = "markdown_style", ext = ".tex", args = c("--top-level-division=chapter",                                                                                                                            rmarkdown::pandoc_latex_engine_args(latex_engine))), clean_supporting = FALSE)
+  rmarkdown::output_format(rmarkdown::knitr_options("html", chapter),
+                           rmarkdown::pandoc_options(to = "latex",
+                                                     from = "markdown_style",
+                                                     ext = ".tex",
+                                                     args = c("--top-level-division=chapter",
+                                                              rmarkdown::pandoc_latex_engine_args(latex_engine))
+                                                     ),
+                           clean_supporting = FALSE)
 }
 
 path <- commandArgs(trailingOnly = TRUE)
